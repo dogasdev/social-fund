b802ee66772cad0376a555a31669ad8be03841e8
Maria Eduarda <meeduardasalencar@gmail.com>
2026-06-01 18:32:40 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        exibirMenu();
+
+    }
+
+    public static void exibirMenu(){
+        System.out.println("╔══════════════════════════════════════════════════════╗");
+        System.out.println("║          FINANCIAMENTO SOCIAL - MENU PRINCIPAL       ║");
+        System.out.println("╠══════════════════════════════════════════════════════╣");
+        System.out.println("(1) Cadastrar Projeto Social \n(2)");
+    }
+}

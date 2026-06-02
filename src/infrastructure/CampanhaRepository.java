2811cbb295b475b3eb155c076e0664349b9e7313
Maria Eduarda <meeduardasalencar@gmail.com>
2026-06-02 11:03:15 -0300

    att

--- src/infrastructure/CampanhaRepository.java
+++ src/infrastructure/CampanhaRepository.java
@@ -1,4 +1,24 @@
 package infrastructure;
+import domain.entities.Campanha;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CampanhaRepository {
+    private final List<Campanha> campanhas = new ArrayList<>();
+
+    public void salvarCampanha(Campanha campanha){
+        campanhas.add(campanha);
+    }
+
+    public List<Campanha> listarCampanhas(){
+        return campanhas;
+    }
+
+    public Campanha buscarCampanhaPorNome(String nome){
+        for(Campanha campanha : campanhas){
+            if(campanha.getNome().equalsIgnoreCase(nome));
+            return campanha;
+        }
+        return null;
+    }
 }
